2e97c3245e3d8aae2cf5c087a44385704b2d6cd1
Shi Guangchuan <shiguangchuan@4paradigm.com>
2022-06-28 12:20:30 +0800

    rename

--- script/generate_iptv.sh
+++ script/generate_iptv.sh
@@ -1,19 +1,25 @@
-
 #!/bin/bash
 
 set -e
 set -o pipefail
 set -x
 
+function transform() {
+    curl --silent --fail --insecure "${1}" \
+    | awk '{
+        if ($0 ~ /^rtp:\/\//) {
+            gsub(/\:\/\//, "/");
+            print "http://192.168.10.1:8686/"$0;
+        } else {
+            print $0;
+        }
+    }' > "${2}"
+}
+
 mkdir -p ./iptv
-curl --silent --fail https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom-Multicast.m3u \
-| gawk '{
-    if ($0 ~ /^rtp:\/\//) {
-        sub(/\:\/\//, "/");
-        print "http://192.168.10.1:8686/"$0;
-    } else {
-        print $0;
-    }
-}' > ./iptv/iptv.m3u
+
+transform "https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom-Multicast.m3u" "./iptv/bjuc_iptv.m3u"
+#transform "https://iptv.im2k.com/tjuc.m3u" "./iptv/tjuc_iptv.m3u"
 
 ls -l ./iptv/
+
